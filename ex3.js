184d902214702128af92e1286a33c25bfbda8114
Denissevar <dvargasvel@gmail.com>
2025-03-06 21:23:37 +0100

    ex3.js

--- ex3.js
+++ ex3.js
@@ -1,10 +1,10 @@
 /*
 1. Comprueba que no se muestra nada por el terminal cuando ejecutamos "node ex3.js"
 2. Asigna un valor a la variable "numero" para que se muestren por el terminal TODOS los console.log del código. Actualmente, estamos asignando un 0.
-3. BONUS: ¿Cual es el valón mínimo entero que resuelve este ejercicio?
+3. BONUS: ¿Cual es el valón mínimo entero que resuelve este ejercicio? 
 */
 
-let numero = 0;
+let numero = 1;
 
 if (numero > 0) {
   console.log("El número es mayor de 0.");
@@ -17,3 +17,11 @@ if (numero > 100) {
 if (numero > 500) {
   console.log("El número es mayor de 500.");
 }
+
+
+
+/*
+1. Comprueba que no se muestra nada por el terminal cuando ejecutamos "node ex3.js" --- comprobado---
+2. Asigna un valor a la variable "numero" para que se muestren por el terminal TODOS los console.log del código. Actualmente, estamos asignando un 0.
+3. BONUS: ¿Cual es el valón mínimo entero que resuelve este ejercicio? -- El valor minimo es 0---
+*/
