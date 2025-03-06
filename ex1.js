8c9c09e0be09fba8ccd98b3bb78023abc82cbbfd
Denissevar <dvargasvel@gmail.com>
2025-03-06 21:13:26 +0100

    ex1.js

--- ex1.js
+++ ex1.js
@@ -2,8 +2,16 @@
 ¿Que va a mostrar por el terminal cuando ejecutemos node ex1.js? ¿Por qué?
 */
 
+/**quiero saber si el numero 4 me sale perro o gato */
 if (1 < 2) {
   console.log("Miau!");
 } else {
+console.log("Guau!");
+}
+
+let numero = 4;
+if (numero === 4) {
   console.log("Guau!");
+} else {
+  console.log("Miau!");
 }
